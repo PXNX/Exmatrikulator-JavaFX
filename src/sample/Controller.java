78f70a309cbb70dffcd02f4a92b854c99b8e03bd
pentexnyx <loool1912@web.de>
2021-04-30 09:22:59 +0200

    Added some very basic layout with events

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button bt_submit;
+    @FXML
+    private Label la_output;
+    @FXML
+    private TextField tf_input;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("uwu wtf");
+
+        bt_submit.setOnAction(e -> la_output.setText("You are " + tf_input.getText() + " years old owo"));
+    }
 }
